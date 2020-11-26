6a6f268bf02790fc9339892452b90524069325d9
Kitsun <kitsunies@gmail.com>
2020-11-27 10:08:25 +1100

    Added list.stringify, swapped " with '.

--- list.lua
+++ list.lua
@@ -1,8 +1,5 @@
 local list = {}
 
-local pairs, ipairs, rawget = pairs, ipairs, rawget
-local select, unpack = select, unpack or table.unpack
-
 -- count the keys in a table with an optional upper limit.
 function list.count(t, maxn)
     local maxn = maxn or 1 / 0
@@ -31,9 +28,9 @@ function list.keys(t, cmp)
     for k in pairs(t) do
         dt[#dt + 1] = k
     end
-    if cmp == true or cmp == "asc" then
+    if cmp == true or cmp == 'asc' then
         table.sort(dt)
-    elseif cmp == "desc" then
+    elseif cmp == 'desc' then
         table.sort(
             dt,
             function(a, b)
@@ -58,7 +55,7 @@ end
 
 -- update a table with the contents of other table(s).
 function list.update(dt, ...)
-    for i = 1, select("#", ...) do
+    for i = 1, select('#', ...) do
         local t = select(i, ...)
         if t then
             for k, v in pairs(t) do
@@ -71,7 +68,7 @@ end
 
 -- add the contents of other table(s) without overwrite.
 function list.merge(dt, ...)
-    for i = 1, select("#", ...) do
+    for i = 1, select('#', ...) do
         local t = select(i, ...)
         if t then
             for k, v in pairs(t) do
@@ -101,18 +98,18 @@ if table.pack then
     list.pack = table.pack
 else
     table.pack = function(...)
-        return {n = select("#", ...), ...}
+        return {n = select('#', ...), ...}
     end
 end
 
---always use this because unpack/table.unpack's default j is #t not t.n.
+-- always use this because unpack/table.unpack's default j is #t not t.n.
 function list.unpack(t, i, j)
-    return unpack(t, i or 1, j or t.n or #t)
+    return (unpack or table.unpack)(t, i or 1, j or t.n or #t)
 end
 
 -- extend a list with the elements of other lists.
 function list.extend(dt, ...)
-    for j = 1, select("#", ...) do
+    for j = 1, select('#', ...) do
         local t = select(j, ...)
         if t then
             local j = #dt
@@ -127,7 +124,7 @@ end
 -- append non-nil arguments to a list.
 function list.append(dt, ...)
     local j = #dt
-    for i = 1, select("#", ...) do
+    for i = 1, select('#', ...) do
         dt[j + i] = select(i, ...)
     end
     return dt
@@ -159,8 +156,9 @@ function list.remove(t, i, n)
     end
 end
 
+-- find the element v from a list t.
 function list.find(t, v)
-    if not v or not t then
+    if v == nil or t == nil then
         return
     end
     for i = 1, #t do
@@ -168,6 +166,16 @@ function list.find(t, v)
             return t[i]
         end
     end
+end
+
+-- check whether t this an array?
+function list.isarray(t)
+    for i in pairs(t) do
+        if type(i) ~= 'number' then
+            return false
+        end
+    end
+    return true
 end
 
 -- shift all the elements on the right of i (i inclusive) to the left or further to the right.
@@ -184,14 +192,14 @@ end
 function list.map(t, f, ...)
     local dt = {}
     if #t == 0 then
-        if type(f) == "function" then
+        if type(f) == 'function' then
             for k, v in pairs(t) do
                 dt[k] = f(k, v, ...)
             end
         else
             for k, v in pairs(t) do
                 local sel = v[f]
-                if type(sel) == "function" then
+                if type(sel) == 'function' then
                     dt[k] = sel(v, ...)
                 else -- field to pluck
                     dt[k] = sel
@@ -199,14 +207,14 @@ function list.map(t, f, ...)
             end
         end
     else
-        if type(f) == "function" then
+        if type(f) == 'function' then
             for i, v in ipairs(t) do
                 dt[i] = f(v, ...)
             end
         else
             for i, v in ipairs(t) do
                 local sel = v[f]
-                if type(sel) == "function" then
+                if type(sel) == 'function' then
                     dt[i] = sel(v, ...)
                 else
                     dt[i] = sel
@@ -215,6 +223,79 @@ function list.map(t, f, ...)
         end
     end
     return dt
+end
+
+--pretty-print t without deep recursion causing a stack overflow
+function list.stringify(t, i)
+    local i = string.rep(' ', i or 4)
+    local cache, stack, output = {}, {}, {}
+    local depth = 1
+    local output_str = '{\n'
+
+    while true do
+        local size = list.count(t)
+
+        local cur_index = 1
+        for k, v in pairs(t) do
+            if cache[t] == nil or cur_index >= cache[t] then
+                if string.find(output_str, '}', #output_str) then
+                    output_str = output_str .. ',\n'
+                elseif string.find(output_str, '\n', #output_str) == nil then
+                    output_str = output_str .. '\n'
+                end
+
+                table.insert(output, output_str)
+                output_str = ''
+
+                local key
+                if type(k) == 'number' or type(k) == 'boolean' then
+                    key = '[' .. tostring(k) .. ']'
+                else
+                    key = '[\'' .. tostring(k) .. '\']'
+                end
+
+                if type(v) == 'number' or type(v) == 'boolean' then
+                    output_str = output_str .. string.rep(i, depth) .. key .. ' = ' .. tostring(v)
+                elseif type(v) == 'table' then
+                    output_str = output_str .. string.rep(i, depth) .. key .. ' = {\n'
+                    table.insert(stack, t)
+                    table.insert(stack, v)
+                    cache[t] = cur_index + 1
+                    break
+                else
+                    output_str = output_str .. string.rep(i, depth) .. key .. ' = \'' .. tostring(v) .. '\''
+                end
+
+                if cur_index == size then
+                    output_str = output_str .. '\n' .. string.rep(i, depth - 1) .. '}'
+                else
+                    output_str = output_str .. ','
+                end
+            else
+                if cur_index == size then
+                    output_str = output_str .. '\n' .. string.rep(i, depth - 1) .. '}'
+                end
+            end
+
+            cur_index = cur_index + 1
+        end
+
+        if size == 0 then
+            output_str = output_str .. '\n' .. string.rep(i, depth - 1) .. '}'
+        end
+
+        if #stack > 0 then
+            t = stack[#stack]
+            stack[#stack] = nil
+            depth = cache[t] == nil and depth + 1 or depth - 1
+        else
+            break
+        end
+    end
+
+    table.insert(output, output_str)
+
+    return table.concat(output)
 end
 
 return list
